1363d7fab93c83d5aa18676e874b74c73c3ff976
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-22 17:25:05 +0900

    jpa 로 데이터 저장하기

---
+++ src/main/java/com/weather/repository/JpaMemoRepository.java
@@ -0,0 +1,9 @@
+package com.weather.repository;
+
+import com.weather.domain.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaMemoRepository extends JpaRepository<Memo, Integer> {
+}
