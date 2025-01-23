7a28a26abf31055e0671bf20c5fac1fe9a3eb3bc
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-23 22:45:07 +0900

    캐싱 기능 적용, 데이터 파싱 과정 교체

---
+++ src/main/java/com/weather/repository/DateWeatherRepository.java
@@ -0,0 +1,14 @@
+package com.weather.repository;
+
+import com.weather.domain.DateWeather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+
+    List<DateWeather> findAllByDate(LocalDate localDate);
+}
