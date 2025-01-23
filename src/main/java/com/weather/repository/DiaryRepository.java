1566dae380018da7d5b97624bb0355ad72c4793c
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-23 20:55:10 +0900

    파싱한 api 데이터 일기와 함께 db에 저장하기

--- src/main/java/com/weather/repository/DiaryRepository.java
+++ src/main/java/com/weather/repository/DiaryRepository.java
@@ -1,8 +1,9 @@
 package com.weather.repository;
 
+import com.weather.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DiaryRepository {
+public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 }
