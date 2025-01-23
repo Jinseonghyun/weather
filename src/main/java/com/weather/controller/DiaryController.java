2ed75ee246d384cc241bc3b8e6cf3f3c28d82351
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-24 01:19:41 +0900

    Api 문서화

--- src/main/java/com/weather/controller/DiaryController.java
+++ src/main/java/com/weather/controller/DiaryController.java
@@ -3,12 +3,17 @@ package com.weather.controller;
 import com.weather.domain.Diary;
 import com.weather.error.InvalidDate;
 import com.weather.service.DiaryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "날씨 일기 API", description = "날씨 일기 관련 컨트롤러")
 @RestController
 public class DiaryController {
 
@@ -18,11 +23,13 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @Operation(summary = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장")
+    @ApiResponse(responseCode = "200", description = "일기 생성 성공")
     @PostMapping("/create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
-
+    @Operation(summary = "선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         if (date.isAfter(LocalDate.ofYearDay(2026, 1))) {
@@ -31,6 +38,8 @@ public class DiaryController {
         return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "선택한 기간 중의 일기 데이터를 가져옵니다.")
+    @Parameter(description = "날짜 형식 : yyyy-MM-dd", example = "2025-01-24")
     @GetMapping("/read/diaries")
     List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                           @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
@@ -46,5 +55,4 @@ public class DiaryController {
     void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         diaryService.deleteDaiary(date);
     }
-
 }
